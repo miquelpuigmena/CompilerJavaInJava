ce24334fca88cbd5e7b9879a33f90bbf72ace78f
miquelpuigmena <miquelpuigmena@gmail.com>
2019-09-11 16:20:54 +0200

    RecursiveDescentCompiler class created

---
+++ Lab1/A1-MiniS/src/java/lang/RecursiveDescentCompiler.java
@@ -0,0 +1,56 @@
+//package lang;
+
+import lang.ast.LangParser;
+import lang.ast.LangScanner;
+import static lang.ast.LangParser.Terminals.*;
+
+/**
+ * Abstract base class for recursive decent parsers.
+ * You should implement the parseProgram() method to parse a MiniS program.
+ */
+public abstract class RecursiveDescentCompiler {
+    private LangScanner scanner;
+    private beaver.Symbol currentToken;
+
+    /** Initialize the parser and start parsing via the parseProgram() method. */
+    public void parse(LangScanner scanner) {
+        this.scanner = scanner;
+        parseProgram();
+        accept(EOF);
+    }
+
+    protected abstract void parseProgram() {
+        //TODO
+        return;
+    }
+
+    /** Returns the current token without proceeding to the next. */
+    protected int peek() {
+        if (currentToken == null) accept();
+        return currentToken.getId();
+    }
+
+    /** Read the next token from the scanner. */
+    protected void accept() {
+        try {
+            currentToken = scanner.nextToken();
+        } catch (Exception e) {
+            error(e.getMessage());
+        }
+    }
+
+    /** Ensure the current token is of a certain type; then read the next. */
+    protected void accept(int expectedToken) {
+        if (peek() != expectedToken) {
+            error("expected token " +
+                    LangParser.Terminals.NAMES[expectedToken] +
+                    " got token " +
+                    LangParser.Terminals.NAMES[currentToken.getId()]);
+        }
+        accept();
+    }
+
+    protected static void error(String message) {
+        throw new RuntimeException(message);
+    }
+}
